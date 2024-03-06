bdb0b92d06427e6672aa60fd32e04462ac6bd6d9
pratikhinge3102001 <pratikhinge2001@gmail.com>
2024-03-06 11:16:35 +0530

    files added suceessfully

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import PhoneOtpForm from './components/phone-login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login With Number</h1>
+      <PhoneOtpForm />
+    </div>
+  );
+}
+
+export default App;
